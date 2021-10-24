0e1870e75a3975a7772c95f79942bf00306b5603
kafamem <kafamem@gmail.com>
2021-10-24 20:39:28 +0000

    Customized admin

--- products/admin.py
+++ products/admin.py
@@ -3,5 +3,25 @@ from .models import Product, Category
 
 # Register your models here.
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+        'is_discount',
+        'discount_price',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
 admin.site.register(Product)
 admin.site.register(Category)
