47f1b73b9cafc5bd551eea2050ea95fd1b7cc3cc
kafamem <kafamem@gmail.com>
2021-10-27 15:10:47 +0000

    Added filtering fnct for categories

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 
@@ -10,6 +10,7 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    categories = None
 
     if request.GET:
         if 'q' in request.GET:
@@ -21,9 +22,15 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
     
     return render(request, 'products/products.html', context)
