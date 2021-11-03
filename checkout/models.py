4b04f9ec4ed8fd9c8e0be57f2034b361640d8db2
kafamem <kafamem@gmail.com>
2021-11-03 18:06:15 +0000

    Added Out of Stock logic and updated forms, admin and signals

--- checkout/models.py
+++ checkout/models.py
@@ -74,11 +74,6 @@ class OrderLineItem(models.Model):
             """
             self.lineitem_total = self.product.price * self.quantity
             super().save(*args, **kwargs)
-        
+            
         def __str__(self):
             return f'SKU {self.product.sku} on order {self.order.order_number}'
-        
-        
-        
-
-    
--- msfour_kafagymstore/settings.py
+++ msfour_kafagymstore/settings.py
@@ -48,6 +48,9 @@ INSTALLED_APPS = [
     'cart',
     'checkout',
 
+    # Other
+    'crispy_forms',
+
 ]
 
 MIDDLEWARE = [
@@ -61,6 +64,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'msfour_kafagymstore.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -76,8 +81,13 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allaauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'cart.contexts.cart_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
