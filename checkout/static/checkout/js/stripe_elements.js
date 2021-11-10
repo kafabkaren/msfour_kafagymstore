1f14fe9aa88a30e8e366771c3744fe928bbd08ee
kafamem <kafamem@gmail.com>
2021-11-10 20:42:47 +0000

    Complete order and admin order to be revised

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -42,33 +42,3 @@ card.addEventListener('change', function (event) {
         errorDiv.textContent = '';
     }
 });
-
-// Handle form submit
-var form = document.getElementById('payment-form');
-
-form.addEventListener('submit', function(ev) {
-    ev.preventDefault();
-    card.update({ 'disabled': true});
-    $('#submit-button').attr('disabled', true);
-    stripe.confirmCardPayment(clientSecret, {
-        payment_method: {
-            card: card,
-        }
-    }).then(function(result) {
-        if (result.error) {
-            var errorDiv = document.getElementById('card-errors');
-            var html = `
-                <span class="icon" role="alert">
-                <i class="fas fa-times"></i>
-                </span>
-                <span>${result.error.message}</span>`;
-            $(errorDiv).html(html);
-            card.update({ 'disabled': false});
-            $('#submit-button').attr('disabled', false);
-        } else {
-            if (result.payementIntent.status === 'succeeded') {
-                form.submit();
-            }
-        }
-    });
-});
