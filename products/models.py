a2cecc3c2aa2653572ff648d67f4a63c8279af61
kafamem <kafamem@gmail.com>
2021-11-17 00:58:32 +0000

    Added rating average stars and review counter

--- products/models.py
+++ products/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models import Avg
+from django.db.models import Avg, Count
 
 class Category(models.Model):
     name = models.CharField(max_length=254)
@@ -44,6 +44,13 @@ class Product(models.Model):
             avg = float(reviews['average'])
         return avg
 
+    # Calculating reviews
+    def countReview(self):
+        reviews = ReviewRating.objects.filter(product=self, status=True).aggregate(count=Count('id'))
+        count = 0
+        if reviews['count'] is not None:
+            count = int(reviews['count'])
+        return count
 
 # Review & rating model
 class ReviewRating(models.Model):
