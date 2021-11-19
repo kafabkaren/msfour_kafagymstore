4c9924e051ed29e27b5349db3c35f5f8fe7e1d29
kafamem <kafamem@gmail.com>
2021-11-19 17:20:06 +0000

    Article webpage layout styled

--- blog/models.py
+++ blog/models.py
@@ -17,7 +17,15 @@ class Post(models.Model):
     author = models.ForeignKey(to=User, on_delete=models.CASCADE)
     status = models.IntegerField(choices=STATUS, default=0)
     
-
-
     def __str__(self):
         return self.title + '|' + str(self.author)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    content = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.post.title, self.name)
