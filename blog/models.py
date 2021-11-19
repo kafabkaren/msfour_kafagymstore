01c4c657947eb454d172cb1e0a85903ea3a397a3
kafamem <kafamem@gmail.com>
2021-11-19 21:02:56 +0000

    Wishlist logic removed and general article details revised

--- blog/models.py
+++ blog/models.py
@@ -19,13 +19,3 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title + '|' + str(self.author)
-
-
-class Comment(models.Model):
-    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
-    name = models.CharField(max_length=200)
-    content = models.TextField()
-    date_created = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return '%s - %s' % (self.post.title, self.name)
