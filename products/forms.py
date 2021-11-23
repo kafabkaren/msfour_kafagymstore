c2d60078a3ff7d9abc4ae031503047caf343f5a6
kafamem <kafamem@gmail.com>
2021-11-23 17:35:35 +0000

    Product Admin - Product Form

--- products/forms.py
+++ products/forms.py
@@ -1,7 +1,23 @@
 from django import forms
 from .models import ReviewRating
+from .models import Product, Category
 
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = ReviewRating
         fields = ['subject', 'review', 'rating']
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'text-dark rounded-2'
