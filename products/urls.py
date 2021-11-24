c2a357a1eefda73634f5585ae0655ffb96139c91
kafamem <kafamem@gmail.com>
2021-11-24 05:22:17 +0000

    Product Admin - Deleting Products

--- products/urls.py
+++ products/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('submit_review/<int:product_id>', views.submit_review, name = 'submit_review'),
     path('add/', views.add_product, name='add_product'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
 ]
