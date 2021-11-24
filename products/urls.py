55ebb37ee92690ab11d7280940642de9eb224da1
kafamem <kafamem@gmail.com>
2021-11-24 04:51:09 +0000

    Product Admin - Editing products

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('<int:product_id>/', views.product_detail, name='product_detail'),
     path('submit_review/<int:product_id>', views.submit_review, name = 'submit_review'),
     path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
 ]
