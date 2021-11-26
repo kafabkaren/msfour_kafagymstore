686cc4f3d9ceec83a93450fafe42b59a286efbde
kafamem <kafamem@gmail.com>
2021-11-26 07:43:30 +0000

    Fix: Attach user's profile to the order and footer product links styled

--- checkout/views.py
+++ checkout/views.py
@@ -146,9 +146,9 @@ def checkout_success(request, order_number):
 
     if request.user.is_authenticated:
         profile = UserProfile.objects.get(user=request.user)
-    # Attach the user's profile to the order
-    order.user_profile = profile
-    order.save()
+        # Attach the user's profile to the order
+        order.user_profile = profile
+        order.save()
 
     # Save the user's info
     if save_info:
