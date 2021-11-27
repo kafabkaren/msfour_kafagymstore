e7be7d3412215924fa7e715a0021b520691b667f
kafamem <kafamem@gmail.com>
2021-11-27 17:15:51 +0000

    Deployment part2

--- msfour_kafagymstore/settings.py
+++ msfour_kafagymstore/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-(-)qyik^^&isr=w10@c@&ug9dbzk%fv444s_#dvk&m3c5@81c!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['msfour_kafagymstore.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -123,12 +123,17 @@ WSGI_APPLICATION = 'msfour_kafagymstore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 
