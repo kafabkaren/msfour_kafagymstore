28e86eee2bfa79404b40a7fcf7409c6aa761daa3
kafamem <kafamem@gmail.com>
2021-12-24 15:40:28 +0000

    AWS S3 region name updated

--- msfour_kafagymstore/settings.py
+++ msfour_kafagymstore/settings.py
@@ -189,7 +189,7 @@ if 'USE_AWS' in os.environ:
     
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'msfour-kafagymstore'
-    AWS_S3_REGION_NAME = 'us-east-1'
+    AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
